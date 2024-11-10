3b85a1c45190a613603909c18a75300ac6cfbf80
tacogips <me@tacogips.me>
2024-11-10 16:46:31 +0900

    add

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,6 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
 const std = @import("std");
+
+const cli = @import("cli.zig");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
